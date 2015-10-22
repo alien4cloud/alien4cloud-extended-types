55a0a92045b2bf614bf279e85ab4d4b152f8308c
Luc Boutier <luc@fastconnect.fr>
2015-10-22 19:27:26 +0200

    [ALIEN-1200] Fixing mount and unmount scripts so they are not Cloudify specific.

--- alien-extended-storage-types-1.0-SNAPSHOT/scripts/mount.sh
+++ alien-extended-storage-types-1.0-SNAPSHOT/scripts/mount.sh
@@ -7,9 +7,9 @@ if [ ! -f ${fs_mount_path} ]; then
     sudo mkdir -p ${fs_mount_path}
 fi
 
-ctx logger info "Mounting file system ${filesys} on ${fs_mount_path}"
+echo "Mounting file system ${filesys} on ${fs_mount_path}"
 sudo mount ${filesys} ${fs_mount_path}
 
 user=$(whoami)
-ctx logger info "Changing ownership of ${fs_mount_path} to ${user}"
+echo "Changing ownership of ${fs_mount_path} to ${user}"
 sudo chown ${user} ${fs_mount_path}
--- alien-extended-storage-types-1.0-SNAPSHOT/scripts/unmount.sh
+++ alien-extended-storage-types-1.0-SNAPSHOT/scripts/unmount.sh
@@ -2,8 +2,8 @@
 
 fs_mount_path=${FS_MOUNT_PATH}
 
-ctx logger info "Unmounting file system on ${fs_mount_path}"
+echo "Unmounting file system on ${fs_mount_path}"
 sudo umount -l ${fs_mount_path}
 
-ctx logger info "Removing ${fs_mount_path} directory"
+echo "Removing ${fs_mount_path} directory"
 sudo rmdir ${fs_mount_path}
