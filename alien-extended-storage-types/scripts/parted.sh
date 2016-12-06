77aec0ec7a5cdc47355335e6890f12c300f55dc4
Louis de Vandiere <louis.devandiere@fastconnect.fr>
2016-12-06 15:48:35 +0100

    UPDATE - manage disks larger than 2TB

---
+++ alien-extended-storage-types/scripts/parted.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+partition_number=1
+device_name=${DEVICE}
+
+echo "Checking existing partition for $device_name"
+sudo parted --script $device_name print 2>/dev/null | grep "Partition Table: unknown"
+PARTITION_UNKNOWN=$(echo $?)
+if [ $PARTITION_UNKNOWN -eq 0 ] ; then
+    echo "Creating disk partition gpt on device ${device_name}"
+    sudo parted --script $device_name \
+        mklabel gpt \
+        mkpart primary 0% 100%
+else
+    echo "Not partitioning device since a partition already exist"
+fi
+
+# Set this runtime property on the source (the filesystem)
+# its needed by subsequent scripts
+# ctx source instance runtime-properties filesys ${device_name}${partition_number}
+export PARTITION_NAME=${device_name}${partition_number}
